6291cce497345a8abcc034c34bdb343c431cd8f7
Arne Decant <hello@arnedecant.be>
2020-07-27 20:34:17 +0200

    test Field component for methods and label value

--- tests/unit/components/base/Field.spec.js
+++ tests/unit/components/base/Field.spec.js
@@ -3,12 +3,23 @@ import Field from '@/components/base/Field.vue'
 
 describe('Field.vue', () => {
     
-    // it('has a change method', () => {
-    //     expect(typeof Field.methods.change).toBe('function')
-    // })
+    it('has an input method', () => {
+        expect(typeof Field.methods.input).toBe('function')
+    })
 
-    // it('has a getClass method', () => {
-    //     expect(typeof Field.methods.getClass).toBe('function')
-    // })
+    it('has a getClass method', () => {
+        expect(typeof Field.methods.getClass).toBe('function')
+    })
+
+    it('renders props.label when passed', () => {
+
+		const label = 'Field label'
+		const wrapper = shallowMount(Field, {
+			propsData: { label }
+        })
+        
+        expect(wrapper.text()).toMatch(label)
+        
+	})
 
 })
--- tests/unit/components/forms/LoginForm.spec.js
+++ tests/unit/components/forms/LoginForm.spec.js
@@ -1,6 +1,12 @@
 import { shallowMount } from '@vue/test-utils'
 import LoginForm from '@/components/forms/LoginForm.vue'
 
+// Mount the component
+
+// const wrapper = shallowMount(LoginForm)
+
+// Describe tests
+
 describe('LoginForm.vue', () => {
     
     it('has a submit method', () => {
